692a80c825d3812e3cfcb14c661adde3973fe59d
Ema <emanuele.piantoni@studiofarma.it>
2014-07-23 17:25:56 +0200

    implementazione direttiva

--- client/js/directives/google-login.js
+++ client/js/directives/google-login.js
@@ -5,5 +5,38 @@
 
 angular.module('remoteStorageApp')
 	.directive('rsGoogleLogin', [function(){
-		return {};
+		
+		//stampo nel dom il file js di google
+		function printJs(){
+			var po = document.createElement('script'); 
+			po.type = 'text/javascript'; 
+			po.async = true;
+    		po.src = 'https://apis.google.com/js/client:plusone.js?onload=render';
+    		var s = document.getElementsByTagName('script')[0]; 
+    		s.parentNode.insertBefore(po, s);
+		};
+
+		function addMeta(name, content){
+			var meta = document.createElement('meta');
+			meta.name = name;
+			meta.content = content;
+			document.getElementsByTagName('head')[0].appendChild(meta);
+		};
+
+		return {
+			restrict: "E", //sta per element -> tag
+			compile: function(element, attributes){
+				//controllo se lo script era già stato inserito
+				if(!window.gapi){
+					addMeta('google-signin-clientid', attributes.clientId);
+        			addMeta('google-signin-scope', 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email');
+        			addMeta('google-signin-requestvisibleactions', 'http://schema.org/AddAction');
+        			addMeta('google-signin-cookiepolicy', 'single_host_origin');
+					printJs();
+				}else {
+					window.render();
+				}
+			}
+
+		};
 }]);
--- client/test/directives/google-login.js
+++ client/test/directives/google-login.js
@@ -8,4 +8,31 @@ describe('googleLogin', function(){
 		});
 	});
 
+	describe('the boot phase', function(){
+		var $compile;
+		var clientId = '964626608963-elde8h4alm10o3kh4t9c1d7o0ehiagfv.apps.googleusercontent.com';
+		var markup = '<rs-google-login current-user="username" client-id="' + clientId +'">';
+
+		beforeEach(function(){
+			inject(function(_$compile_){ //gli _ sono invarianti
+				$compile = _$compile_;
+			});
+		});
+
+		//se passo l'argomento done, mocha capisce che il test è asincrono
+		it('calls the window.render function', function(done){
+			window.render = function(){ done(); };
+			$compile(markup);
+		});
+
+		it('attach the required meta tags', function(done){
+			window.render = function(){
+				var meta = document.querySelector('meta[name="google-signin-clientid"]');
+				expect(meta.content).to.be.equal(clientId);
+				done();
+			};
+			$compile(markup);
+		});
+	});
+
 });
