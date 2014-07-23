45435acc9200d156bd9e38f8d048f09e86b26746
Ema <emanuele.piantoni@studiofarma.it>
2014-07-23 12:27:16 +0200

    setup test client

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ app.get('/', function(req, res){
 	res.send('Hello world');
 });
 
+//questo significa che quando la rotta indentifica un file su disco, restituisce il file
+app.use(express.static(__dirname + '/../client'));
+
 //callback viene lanciata quando il server è run
 var server = app.listen(3000, function(){
 	console.log('Listening on port 3000'); 
